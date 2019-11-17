5f8369f3533d052c8a794f16a838c18146aa61d7
Jiss James <jissjames360@gmail.com>
2019-11-17 23:07:22 +0000

    index tables

--- mysite/reviews/views.py
+++ mysite/reviews/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import numpy as np
+
 
 # Create your views here.
 def index(request):
 
-    return render(request, 'index.html')
+    # read form excel as dataframe
+    df = pd.read_excel('../bunsen_reviews.xls', sheet_name='Sheet1')
+    # no need to display sentimental analysis and no.of reviews, so drop it
+    df = df.iloc[:, :-2]
+
+    print("Column headings:")
+    print(df.columns)
+
+    # add null to cells with empty values
+    df['Name'].replace('', np.nan, inplace=True)
+    df['Review'].replace('', np.nan, inplace=True)
+    df['Date'].replace('', np.nan, inplace=True)
+
+    df.dropna(subset=['Name'], inplace=True)
+    df.dropna(subset=['Review'], inplace=True)
+    df.dropna(subset=['Date'], inplace=True)
+
+    # Create an empty list
+    Row_list = []
+
+    # Iterate over each row
+    for rows in df.itertuples():
+        # Create list for the current row
+        my_list = [rows.Name, rows.Review, rows.Date]
+
+        # append the list to the final list
+        Row_list.append(my_list)
+
+    # Print the list
+    print(Row_list)
+
+    # pass the list to template
+
+    return render(request, 'index.html', {"response": Row_list})
